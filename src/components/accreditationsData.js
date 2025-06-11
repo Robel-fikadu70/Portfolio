631ded832d39a9b6056dd791dfc12aa2d5467db8
Ruther.f <robelfikadu70@gmail.com>
2025-06-12 02:55:18 +0300

    edited image path

--- src/components/accreditationsData.js
+++ src/components/accreditationsData.js
@@ -4,37 +4,37 @@ export const accreditationsData = [
     id: 1,
     date: "Jan 05, 2024",
     description: "Python Programming - AAiT (Addis Ababa University Institution Of Technology)",
-    image: "src/assets/pyhonAAiT.jpg", // Update path as needed
+    image: "/assets/pyhonAAiT.jpg", // Update path as needed
   },
   {
     id: 2,
     date: "Feb 10, 2024",
     description: "Artificial Intelligence - AAiT (Addis Ababa University Institution Of Technology)",
-    image: "src/assets/Ai_AAiT.jpg",
+    image: "/assets/Ai_AAiT.jpg",
   },
   {
     id: 3,
     date: "Sept 24, 2024",
     description: "Artificial Intelligence Fundamentals - Udemy Nanodegree Program ",
-    image: "src/assets/AI_udacity.jpg",
+    image: "/assets/AI_udacity.jpg",
   },
   {
     id: 4,
     date: "Oct 4, 2024",
     description: "Programming Fundamentals - Udemy Nanodegree Program",
-    image: "src/assets/Programming_Fundamentals_udacity.jpg",
+    image: "/assets/Programming_Fundamentals_udacity.jpg",
   },
   {
     id: 5,
     date: "Nov 22, 2024",
     description: "Data Analysis Fundamentals - Udemy Nanodegree Program",
-    image: "src/assets/DataAnlysis_udacity.jpg",
+    image: "/assets/DataAnlysis_udacity.jpg",
   },
   {
     id: 6,
     date: "Jan 12, 2025",
     description: "Android Developer Fundamentals - Udemy Nanodegree Program",
-    image: "src/assets/Android_udacity.jpg",
+    image: "/assets/Android_udacity.jpg",
   },
   // Add 4 more entries...
 ];
